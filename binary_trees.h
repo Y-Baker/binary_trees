809846f7fba54060715b481fb838fdc37f9409f1
Yousef <yuossefbakier@gmail.com>
2023-08-31 14:04:12 +0300

    Update Header

--- binary_trees.h
+++ binary_trees.h
@@ -1,9 +1,10 @@
-#ifndef B_S_T_H
-#define B_S_T_H
-
+#ifndef MAIN_H
+#define MAIN_H
+								/*HEADERS*/
 #include <stdio.h>
 #include <stdlib.h>
 
+								/*STRUCTURE*/
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -12,15 +13,17 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-struct binary_tree_s
+typedef struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
-};
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+} binary_tree_t;
 
-typedef struct binary_tree_s binary_tree_t;
+								/*DEFINATIONS*/
+
+								/*PROTOTYPE*/
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
@@ -43,4 +46,5 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 	const binary_tree_t *second);
-#endif
+
+#endif /*MAIN_H*/
